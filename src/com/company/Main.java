ba72bad2bcde42a2e349279fa408494ffa0193dd
swy20190 <K201410109@163.com>
2020-12-19 21:17:00 +0800

    fix bugs, add the minimum standard deviation.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import org.apache.commons.math3.distribution.NormalDistribution;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        SlideWindow slideWindow = new SlideWindow(10);
+//        for(int i=0; i<200;i+=10){
+//            System.out.println(slideWindow.phi(i));
+//            slideWindow.addTime(i);
+//        }
+        NormalDistribution normalDistribution = new NormalDistribution(1545849.75 , 22387.405794050814);
 
     }
 }
